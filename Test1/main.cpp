42366efb4461850930a8381a77fe4aca711bba3b
Fernando G. Testa <fernando.testa@gmail.com>
2015-09-10 16:19:20 +0100

    Initial Commit

---
+++ Test1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test1
+//
+//  Created by Fernando Testa on 10/09/2015.
+//  Copyright (c) 2015 Fernando Testa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
