dc2f00bc120354745c85e5cd66d88e9e0f0c4121
Fernando G. Testa <fernando.testa@gmail.com>
2015-10-13 10:47:14 +0100

    Removed source Lua: will use system distribution.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
 #include "LuaUtils.hpp"
+#include <ev.h>
 #include <math.h>
+#include <iostream>
+#include <thread>
+#include <vector>
+
 /*
  Check http://www.rasterbar.com/products/luabind/docs.html 
  for more sofisticated C++ to Lua binding.
@@ -15,13 +19,13 @@
     return 1;  /* number of results */
 }
 
-int main()
+void run_interpreter(const char * luascript)
 {
     try {
         CLuaInterpreter interpreter;
-        interpreter.register_function("mysin" ,l_sin);
-        interpreter.load_file("test.lua");
-
+        interpreter.register_function("main" ,l_sin);
+        interpreter.load_file(luascript);
+        
         long x,y;
         {
             CLuaCall myfancyfunction(interpreter, "myfancyfunction");
@@ -34,7 +38,23 @@ int main()
     catch(lua_exception& e)
     {
         std::cerr << "LUA EXCEPTION: " << e.what() << std::endl;
-        return -1;
+        return;
     }
+}
+
+int main()
+{
+    const int number_of_threads = 100;
+    std::cout << "thread::hardware_concurrency = " << std::thread::hardware_concurrency() << std::endl;
+    std::vector<std::thread> ts;
+    for(int i=0;i<number_of_threads;i++)
+    {
+        ts.push_back(std::thread(run_interpreter,"test.lua"));
+        
+    }
+
+    for(auto& it : ts)
+        it.join();
+    
     return 0;
-}   
+}
